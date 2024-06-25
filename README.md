0b952ec6d5021e76660de091937651f28bfa4952
Dulio Denis <dulio.denis@yahoo.com>
2024-06-24 23:58:26 -0400

    Initial App commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# userdefaults-wrapper
+# UserDefaults Property Wrapper
 This is a Swift / SwiftUI property wrapper that simplifies working with UserDefaults, allowing developers to use properties that automatically sync with UserDefaults.
---
+++ UserDefaultWrapper/UserDefaultWrapper/UserDefaultWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultWrapperApp.swift
+//  UserDefaultWrapper
+//
+//  Created by Dulio Denis on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
